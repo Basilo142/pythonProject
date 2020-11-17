7fedefec6818b262b5261ec8ff8b33a428d598c1
OLEKSANDR DIADIUN <diadiun@bitbucket.org>
2020-11-17 19:14:52 +0200

    17.11.20

--- Week3/solution.py
+++ Week3/solution.py
@@ -1,9 +1,26 @@
-class FileReader:
-    def __init__(self, file):
-        self._file=file
-    def read(self):
-        try:
-            with open(self._file,'r') as f:
-                return f.read()
-        except IOError:
-            return ''
+class MyClass:
+    def __init__(self):
+        self.x=100
+    def print_x(self,y=None):
+        self.y=y
+        if not y:
+            print(self.x)
+        else:
+            print(self.y)
+
+
+
+
+c=MyClass()
+
+c.print_x(2)
+c.print_x()
+#c.print_x
+print("c.x = ", c.x)
+print("getattr = ", getattr(c, "x"))
+print("getattr = ", getattr(c, "print_x")())
+print(c.x)
+print("delattr = ", delattr(c, "x"))
+#print(c.x)
+c.print_x(2)
+c.print_x()
