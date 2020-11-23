7911d3d727744b9502dbd7eee1ccc6e5f8cc1f70
OLEKSANDR DIADIUN <diadiun@bitbucket.org>
2020-11-23 18:08:32 +0200

    23.11.20

--- Week3/solution.py
+++ Week3/solution.py
@@ -13,17 +13,24 @@ with open(csv_filename) as csv_fd:
 
 class CarBase:
     def __init__(self, brand, photo_file_name, carrying):
-        pass
+        self.brand = brand
+        self.photo_file_name = photo_file_name
+        self.carrying = carrying
 
 
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
-        pass
+        super().__init__(brand, photo_file_name, carrying)
+        self.passenger_seats_count = passenger_seats_count
+
+
 
 
 class Truck(CarBase):
     def __init__(self, brand, photo_file_name, carrying, body_whl):
-        pass
+        super().__init__(brand, photo_file_name, carrying)
+        self.body_whl = body_whl
+
 
 
 class SpecMachine(CarBase):
