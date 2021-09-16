679dd9fcdf3375dfe03573cc1c40443ca2a29491
OLEKSANDR DIADIUN <diadiun@bitbucket.org>
2021-09-16 18:25:01 +0300

    bot

--- Udemy/gippo/app.py
+++ Udemy/gippo/app.py
@@ -1,2 +1,15 @@
-from aiogram import Bot
-bot = Bot(token='3333')
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+bot = Bot(token='***')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def get_mes(message: types.Message):
+
+    chat_id = message.chat.id
+    send = await bot.send_message(chat_id=chat_id, text='asdasd')
+    print(send.to_python())
+
+executor.start_polling(dp)
