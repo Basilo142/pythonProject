fb34c13ad19664ab147eef9cf2be9753ced32c55
oleksandrdiadiun <diadiun.o@ajax.systems>
2023-07-18 18:45:22 +0300

    linter

--- Book/generat.py
+++ Book/generat.py
@@ -1,8 +1,10 @@
 def even_range(start, end):
-    current=start
-    while current<end:
+    current = start
+    while current < end:
         yield current
         print('после yield - {}'.format(current))
-        current +=2
-for number in even_range(0,20):
+        current += 2
+
+
+for number in even_range(0, 20):
     print(number)
--- Book/generat2.py
+++ Book/generat2.py
@@ -3,16 +3,18 @@ def accumulator():
     while True:
         value = yield total
         print('Go: {}'.format(value))
-        if not value: break
-        total+=value
+        if not value:
+            break
+        total += value
 
-generator= accumulator()
+
+generator = accumulator()
 next(generator)
 print("print--- {}".format(generator.send(1)))
 print("print--- {}".format(generator.send(1)))
 print("print--- {}".format(generator.send(1)))
 
-w=(1, 2, 'e')
+w = (1, 2, 'e')
 print(w)
 print(type(w))
 print(type(w[0]))
